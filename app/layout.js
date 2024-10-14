b6f32e48c71d89bf6cba298f484ac347da54fbe1
B-mitchell <mitchellonuorah77@gmail.com>
2024-10-14 12:04:57 +0100

    updates

--- app/layout.js
+++ app/layout.js
@@ -4,7 +4,7 @@ import { ClerkProvider } from '@clerk/nextjs'
 import "./globals.css";
 
 export const metadata = {
-  title: 'Job Reccruitment system',
+  title: 'Job Recruitment system',
   description: 'Built by Big Mitch',
 }
 
